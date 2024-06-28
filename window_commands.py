990e5cc6cc633061bcae6164f629d7fb1b6bbe5e
diogotito <diogotitomarques@gmail.com>
2024-06-28 11:10:11 +0100

    Better names

--- window_commands.py
+++ window_commands.py
@@ -2,8 +2,15 @@ import sublime
 import sublime_plugin
 
 
-class CloseFileWithoutSaving(sublime_plugin.WindowCommand):
+class CloseFileWithoutPrompting(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.active_view().set_scratch(True)
-        self.window.active_view().close()
+        # Mark the active view (tab) as scratch, so that Sublime doesn't
+        # prompt about unsaved changes on close
+        the_tab = self.window.active_view()
+        the_tab.set_scratch(True)
+        the_tab.close()
+
+        # Removing the scratch status brings back the dot indicator in the tab
+        # if there are unsaved changes, and Sublime prompts again for unsaved
+        # changes on close.
         self.window.active_view().set_scratch(False)
